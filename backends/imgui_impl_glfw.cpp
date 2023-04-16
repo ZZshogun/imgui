db5f083e7521bccc65acac937325cb85f242a8de
ZZ19 <rachatakanit@gmail.com>
2023-04-16 16:58:49 +0700

    Updated linking

--- backends/imgui_impl_glfw.cpp
+++ backends/imgui_impl_glfw.cpp
@@ -75,16 +75,16 @@
 #endif
 
 // GLFW
-#include "glfw/include/GLFW/glfw3.h"
+#include "glfw/glfw3.h"
 
 #ifdef _WIN32
 #undef APIENTRY
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include "glfw/include/GLFW/glfw3native.h"   // for glfwGetWin32Window()
+#include "glfw/glfw3native.h"   // for glfwGetWin32Window()
 #endif
 #ifdef __APPLE__
 #define GLFW_EXPOSE_NATIVE_COCOA
-#include "glfw/include/GLFW/glfw3native.h"   // for glfwGetCocoaWindow()
+#include "glfw/glfw3native.h"   // for glfwGetCocoaWindow()
 #endif
 
 #ifdef __EMSCRIPTEN__
